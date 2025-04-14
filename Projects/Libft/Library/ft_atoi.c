51086f88bec14b855ef9578d0539a191dff8bd44
bpco042 <brunojcarvalho23333@gmail.com>
2025-04-14 13:28:45 +0100

    Update ft_atoi.c

--- Projects/Libft/Library/ft_atoi.c
+++ Projects/Libft/Library/ft_atoi.c
@@ -21,25 +21,27 @@ int ft_atoi(const char *str)
     i = 0;
     sign = 1;
     result = 0;
-
+    //#ignores blank spaces and break chars of ascii table between 9 and 13
     while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
         i++;
-    
+    //#treats the signal in case of a negative number
     if (str[i] == '-')
     {
         sign = -1;
         i++;
-    }    
+    }
+    //#or move on if it's a positive one
     else
     {
         i++;
     }
-
+    //#while the str is not ended and it's between 0 and 9, composes an int result with the appropriate numeric char representation of the converted digit
     while (str[i] && str[i] >= '0' && str[i] <= '9')
     {
         result = (result * 10) + (str[i] - '0');
         i++;
     }
+    //#returns the result of the conversion multiplied by the sign
     return (result * sign);
 }
 /*Converts string to integer
