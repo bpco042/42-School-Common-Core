197da0a8f1d4e7b17aaa746bd79498577648a318
bpco042 <brunojcarvalho23333@gmail.com>
2025-04-14 13:30:25 +0100

    Update ft_bzero.c

--- Projects/Libft/Library/ft_bzero.c
+++ Projects/Libft/Library/ft_bzero.c
@@ -14,10 +14,11 @@
 
 void    ft_bzero(void *dest, size_t len)
 {
+    //#uses memset to make the zerofication of the positions
     ft_memset(dest, 0, len);
 }
-/*
-int main() {
+//sets n number of positions into zero
+/*int main() {
     char str[10] = "Hello1234";
 
     printf("before %s\n", str);
