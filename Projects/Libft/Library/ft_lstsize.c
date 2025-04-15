5bd8e9159018218e544718ba7d880184305a000b
bpco042 <brunojcarvalho23333@gmail.com>
2025-04-15 12:13:10 +0100

    Update ft_lstsize.c

--- Projects/Libft/Library/ft_lstsize.c
+++ Projects/Libft/Library/ft_lstsize.c
@@ -17,12 +17,15 @@ int ft_lstsize(t_list *lst)
     int i;
 
     i = 0;
+    // if lst is empty, return 0
     if (!lst)
         return (0);
-
+    // loops through the lst until the end
     while(lst)
     {
+        // count nodes
         i++;
+        // move the pointer to the next node
         lst = lst->next;
     }
     return (i);
