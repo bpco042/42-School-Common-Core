6b3fd9d6cd4c67b45d4ec2a8a74d77d246c8b3b0
bpco042 <brunojcarvalho23333@gmail.com>
2025-04-15 12:31:06 +0100

    Update ft_memmove.c

--- Projects/Libft/Library/ft_memmove.c
+++ Projects/Libft/Library/ft_memmove.c
@@ -16,16 +16,16 @@ void    *ft_memmove(void *dest, const void *src, size_t len)
     size_t i;
     unsigned char *d;
     const unsigned char *s;
-
-    if (!dest && !src)
+    // check if dest and src are null
+    if (!dest || !src)
         return (NULL);
-    
+    // casting to it's possible to work byte-byte
     d = (unsigned char *)dest;
     s = (unsigned char *)src;
-
+    // base case: if it's equal, return dest
     if (d == s)
         return (dest);
-    
+    // if dest is greater than src, copy from the end to avoid overlap
     if (d > s)
     {
         i = len;
@@ -38,6 +38,7 @@ void    *ft_memmove(void *dest, const void *src, size_t len)
     else
     {
         i = 0;
+        // otherwise, copy from the beginning
         while (i < len)
         {
             d[i] = s[i];
