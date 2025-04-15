34b7003dfb6d2c209f9aab2ef1d89fe50995adc8
bpco042 <brunojcarvalho23333@gmail.com>
2025-04-15 12:19:08 +0100

    Update ft_memchr.c

--- Projects/Libft/Library/ft_memchr.c
+++ Projects/Libft/Library/ft_memchr.c
@@ -16,11 +16,13 @@ void *ft_memchr(const void *str, int c, size_t n)
 {
     size_t  i;
     unsigned char   *s;
-
+    // cast of *str to unsigned char for byte comparison
     s = (unsigned char *)str;
     i = 0;
+    // traverse while i is less then n
     while (i < n)
     {
+        // check if the current byte is equal to c
         if (s[i] == (unsigned char)c)
             return ((void *)&s[i]);
         i++;
