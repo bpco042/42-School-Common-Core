acc6cf56f51c5f8e66f4bb0b49b63798dff540e7
bpco042 <brunojcarvalho23333@gmail.com>
2025-04-23 19:54:52 +0100

    update 23-04

---
+++ Projects/get_next_line/get_next_line.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c               		                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bruperei <bruperei@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 14:20:18 by bruperei          #+#    #+#             */
+/*   Updated: 2025/04/23 15:47:34 by bruperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char	*rtn;
+	static char	*str;
+
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	str = ft_line_allocation(fd, str);
+	if (!str)
+		return (NULL);
+	rtn = ft_next_line(str);
+	str = ft_rem_line(str);
+	return (rtn);
+}
+int	main(void)
+{
+	char	*s;
+	int	fd;
+	int	line_nb = 1;
+
+	fd = open("file.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error");
+		return (1);
+	}
+	while (1)
+	{
+		s = get_next_line(fd);
+		if (s == NULL)
+			break;
+		printf("line %d: %s", line_nb, s);
+		free(s);
+		line_nb++;
+	}
+	close(fd);
+	return (0);
+}
