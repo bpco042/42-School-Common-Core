0985e468c313f4188c1a3b4389d5b8b8c80657e2
bpco042 <brunojcarvalho23333@gmail.com>
2025-04-24 19:36:44 +0100

    Update ft_substr.c

--- Projects/Libft/Library/ft_substr.c
+++ Projects/Libft/Library/ft_substr.c
@@ -21,7 +21,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!s)
 		return (NULL);
 	size = ft_strlen(s);
-	// check if there's room available to fit the substr
+	// check if the start position is beyond the size
 	if (start >= size)
 	{
 		// there's no room to fit it, so allocate memory to \0
@@ -31,14 +31,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		substr[0] = '\0';
 		return (substr);
 	}
-	// 
+	// adjust the size of the substr in case it goes beyond the size of the original; else, remains the same
 	if (start + len > size)
 		n_len = size - start;
 	else
 		n_len = len;
+	// allocates memory to the substr + terminator 
 	substr = malloc(sizeof(char) * (n_len + 1));
 	if (!substr)
 		return (NULL);
+	// copy all from the start until whats was defined at n_len
 	ft_memcpy(substr, s + start, n_len);
 	substr[n_len] = '\0';
 	return (substr);
