f8193f79c756f2a1c93144543b8b152b418058d8
bpco042 <brunojcarvalho23333@gmail.com>
2025-04-24 19:01:41 +0100

    Update ft_itoa.c

--- Projects/Libft/Library/ft_itoa.c
+++ Projects/Libft/Library/ft_itoa.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 #include "libft.h"
 
+// count digits so we can allocate the correct amount of memory
 static int	count_digit(long n)
 {
 	int	len;
@@ -34,20 +35,25 @@ char	*ft_itoa(int n)
 	char	*nb;
 	long	num;
 	size_t	len;
-
+	//cast from int to long, so we avoid overflow in the extreme cases
 	num = (long)n;
+	//count the size of the nb + 1, so we can allocate memory
 	len = count_digit(num) + 1;
+	//allocate memory to var nb
 	nb = (char *)malloc(sizeof(char) * len);
 	if (!nb)
 		return (NULL);
+	//handles negative cases
 	if (num < 0)
 	{
 		num = num * -1;
 		nb[0] = '-';
 	}
 	nb[--len] = '\0';
+	// handle explicitly the zero case
 	if (num == 0)
 		nb[0] = '0';
+	// handle positive cases
 	while (num)
 	{
 		nb[--len] = (num % 10) + '0';
