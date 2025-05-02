cda912d374621e9ea1ba409be35f62afe670289f
bpco042 <brunojcarvalho23333@gmail.com>
2025-05-02 21:50:04 +0100

    Update ft_unsigned.c

--- Projects/ft_printf/ft_unsigned.c
+++ Projects/ft_printf/ft_unsigned.c
@@ -12,51 +12,70 @@
 
 #include "ft_printf.h"
 
+// Auxiliary function to calculate the number of digits in an unsigned int
 int	ft_unlen(unsigned int num)
 {
 	int	len;
 
 	len = 0;
+
+	// Loop until all digits are counted
 	while (num != 0)
 	{
 		len++;
-		num = num /10;
+		num = num /10; // Remove the last digit
 	}
+	// Return the number of digits
 	return (len);
 }
 
+// Function to convert an unsigned int to a string
 char	*ft_utoa(unsigned int num)
 {
 	char	*str;
 	int	len;
 
+	// Get the number of digits
 	len = ft_unlen(num);
+
+	// Allocate memory fro the string +1
 	str = malloc((len + 1) * sizeof(char *));
+
+	// Check if the allocation was successful
 	if (!str)
 		return (NULL);
+	// Add a null terminator in the string
 	str[len] = '\0';
+
+	// Convert the number to a string from the end to the beginning
 	while (num != 0)
 	{
-		str[len - 1] = num % 10 + '0';
-		num = num / 10;
+		str[len - 1] = num % 10 + '0'; // Get the last digit and convert it to a char
+		num = num / 10; // Remove the last digit
 		len--;
 	}
-	return (str);
+	return (str); // Return the string
 }
 
+// Main function to print an unsigned int and return the number of chars
 int	ft_unsigned(unsigned int num)
 {
 	int	len;
 	char	*str;
 
 	len = 0;
+
+	// Handle the zero case
 	if (num == 0)
 		len += write(1, "0", 1);
 	else
 	{
+		// Convert number to a string
 		str = ft_utoa(num);
+		// Print the string and store the length
 		len += ft_putstr_len(str);
+		// Free the allocated memory
 		free(str);
 	}
-	return (len);
+	return (len); // Return the total number of chars printed
 }
