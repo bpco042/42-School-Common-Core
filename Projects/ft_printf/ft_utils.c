3c8f8becf95ffbe135f22a5cbf27043a8c110e9b
bpco042 <brunojcarvalho23333@gmail.com>
2025-05-02 22:41:33 +0100

    Update ft_utils.c

--- Projects/ft_printf/ft_utils.c
+++ Projects/ft_printf/ft_utils.c
@@ -12,33 +12,38 @@
 
 #include "ft_printf.h"
 
+// Function to print a single char and return its length (always 1)
 int	ft_putchar_len(char c)
 {
-	write(1, &c, 1);
-	return (1);
+	write(1, &c, 1); // Print an char
+	return (1); // Return the length of the printed char
 }
-//the same putchar, but returning the lenght
 
+// Function to print a string and return its printed length
 int	ft_putstr_len(char *str)
 {
+	// Handle the null case
 	if (!str)
 	{
 		write(1, "(null)", 6);
 		return (6);
 	}
+	// Otherwise, print the current string 
 	write (1, str,ft_strlen(str));
-	return (ft_strlen(str));
+	return (ft_strlen(str)); // Return the number of chars printed
 }
-//the same putstr but returning the lenght
 
+// Function to print an int and return the number of chars printed
 int	ft_putnbr_len(int num)
 {
 	int	len;
 	char	*str;
 
+	// Convert int to a string
 	str = ft_itoa(num);
+	// Print the string and get the number of chars printed
 	len = ft_putstr_len(str);
+	// Free the allocated memory
 	free(str);
-	return (len);
+	return (len); // Return the length of the printed chars
 }
-// putnbr returning the lenght
